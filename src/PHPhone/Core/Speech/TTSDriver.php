0d769f74212917da13584441d7903d27b16ada9b
Mike Knooihuisen <mike@simplysolutionscoding.com>
2018-11-15 09:37:51 -0500

    Basic VoiceRSS WIP

--- src/PHPhone/Core/Speech/TTSDriver.php
+++ src/PHPhone/Core/Speech/TTSDriver.php
@@ -7,7 +7,80 @@
 namespace PHPhone\Core\Speech;
 
 
-interface TTSInterface
+abstract class TTSDriver
 {
+	protected $filename, $text, $voice;
 
+	/**
+	 * @param string $filename the desired ending filename
+	 * @param string $text the text to say
+	 * @param null $voice the voice to use to speak the text
+	 * @throws \Exception if an input is invalid or on generation failure.
+	 * @return bool success or failure
+	 */
+	public function generate($filename, $text, $voice = null) {
+		if($voice == null) {
+			$voice = self::getDefaultVoice();
+		}
+
+		$this->validate($filename, $text, $voice);
+		logger(null, "Made it here");
+		$result = $this->request($filename, $text, $voice);
+
+		if($result) {
+			//TODO: MOdule reload sounds
+		} else {
+			logger()->error("Could not generate audio file.");
+			throw new \Exception("Could not generate audio file.");
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param string $filename the file name to save to
+	 * @param string $text to speak
+	 * @param string $voice the voice to use
+	 * @return bool true on success, else false
+	 */
+	abstract function request($filename, $text, $voice);
+
+	/**
+	 * @return string the default voice for the driver
+	 */
+	abstract function getDefaultVoice();
+
+	/**
+	 * @return array all available voices for the driver
+	 */
+	abstract function getVoices();
+
+	/**
+	 * @return string the key used to identify this driver in commands
+	 */
+	abstract function getKey();
+
+	/**
+	 * @param string $filename the desired filename to save to
+	 * @param string $text the desired speech text
+	 * @param string $voice the desired voice to speak the text
+	 * @return bool true if validated
+	 * @throws \Exception if an input is invalid.
+	 */
+	private function validate($filename, $text, $voice){
+		if($this->text == null || trim($text) == "") {
+			logger()->error("No speech text found.  Aborting!");
+			throw new \Exception("No speech text found.  Aborting!");
+		} else if($filename == null || trim($filename) == "") {
+			logger()->error("Invalid filename.  Aborting!");
+			throw new \Exception("Invalid filename.  Aborting!");
+		} else if(array_search($voice, self::getVoices()) === false) {
+			$msg = "Voice invalid.  Valid voices are: ".
+				implode(", ", self::getVoices());
+			logger()->error($msg);
+			throw new \Exception($msg);
+		}
+
+		return true;
+	}
 }
--- src/PHPhone/Core/Speech/VoiceRSSDriver.php
+++ src/PHPhone/Core/Speech/VoiceRSSDriver.php
@@ -7,7 +7,58 @@
 namespace PHPhone\Core\Speech;
 
 
-class VoiceRSSDriver
+use GuzzleHttp\Client;
+use PHPhone\PHPhone\Speech\VoiceRSS;
+
+class VoiceRSSDriver extends TTSDriver
 {
 
+	public function getVoices()
+	{
+		return [
+			"en-us", "ca-es", "zh-cn", "zh-hk", "zh-tw", "da-dk",
+			"nl-nl", "en-au", "en-ca", "en_gb", "en-in", "fi-fi",
+			"fr-ca", "fr-fr", "de-de", "it-it", "ja-jp", "ko-kr",
+			"nb-no", "pl-pl", "pt-br", "pt-pt", "ru-ru", "es-mx",
+			"es-es", "sv-se"
+		];
+	}
+
+	public function getDefaultVoice()
+	{
+		return "en-us";
+	}
+
+	public function getKey()
+	{
+		return "voicerss";
+	}
+
+	public function request($filename, $text, $voice)
+	{
+		return $this->speech([
+			'key' => env("VR_KEY"),
+			'hl' => $voice,
+			'src' => $text,
+			'r' => '0',
+			'c' => 'mp3',
+			'f' => '44khz_16bit_stereo',
+			'ssml' => 'false',
+			'b64' => 'false'
+		]);
+
+	}
+
+	/**
+	 * @param $settings
+	 * @return string file contents
+	 * @throws \GuzzleHttp\Exception\GuzzleException on request failure
+	 */
+	private function speech($settings) {
+		$client = new Client();
+		$response = $client->request('POST', "https://api.voicerss.org", [
+			'form_params' => [$settings]
+		]);
+		return $response->getBody()->getContents();
+	}
 }
